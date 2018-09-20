2ad5973d5af62bd6b9b3b0f800e575de1641d4d5
Bach <gliheng@foxmail.com>
2018-09-20 21:56:02 +0800

    part1 start

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flutter notebook'),
+      ),
+      body: Center(child: Text('Hello, world!'))
+    );
+  }
+}

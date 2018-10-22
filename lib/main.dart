c477ac56bd9e6c385cc1cd250a42ae2549ac08d0
Bach <gliheng@foxmail.com>
2018-10-22 14:01:55 +0800

    part1 end

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,44 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('flutter notebook'),
       ),
-      body: Center(child: Text('Hello, world!'))
+      body: NoteBookItem(
+        icon: Icon(Icons.star),
+        text: 'Note message',
+        timestamp: DateTime.now()
+      )
+    );
+  }
+}
+
+class NoteBookItem extends StatelessWidget {
+  NoteBookItem({@required this.icon, @required this.text, @required this.timestamp});
+
+  final Icon icon;
+  final String text;
+  final DateTime timestamp;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50.0,
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: icon,
+          ),
+          Expanded(
+            child: Text(text,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2
+            )
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text('${timestamp.month} - ${timestamp.day}'),
+          ),
+        ],
+      ),
     );
   }
 }

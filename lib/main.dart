c41e28543f8e62e4aa7e1fdd9403e3adabba81af
Bach <gliheng@foxmail.com>
2018-10-22 14:40:57 +0800

    part2 end

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,56 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.yellow.shade700
       ),
       home: HomePage()
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<NoteModel> notes = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('flutter notebook'),
       ),
-      body: NoteBookItem(
-        icon: Icon(Icons.star),
-        text: 'Note message',
-        timestamp: DateTime.now()
-      )
+      body: ListView(
+        children: notes.map((note) => NoteBookItem(
+          icon: Icon(note.iconData),
+          text: note.text,
+          timestamp: note.timestamp
+        )).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            notes.add(NoteModel(
+              iconData: Icons.star,
+              text: 'Hello',
+              timestamp: DateTime.now())
+            );
+          });
+        }
+      ),
     );
   }
+}
+
+class NoteModel {
+  NoteModel({this.iconData, this.text, this.timestamp});
+
+  final IconData iconData;
+  final String text;
+  final DateTime timestamp;
 }
 
 class NoteBookItem extends StatelessWidget {
